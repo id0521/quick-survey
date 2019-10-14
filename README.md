a47c0e4296d947db2a20de71104467d01243dd36
ivand521 <ivandong521@gmail.com>
2019-10-14 15:51:51 -0400

    Use express.json

---
+++ README.md
@@ -0,0 +1,2 @@
+To use the app:
+Log in with your Google account and add credit to your account using card number 4242-4242-4242-4242. Create survey by clicking the button on the bottom of the page.
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
 const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./models/Survey');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
